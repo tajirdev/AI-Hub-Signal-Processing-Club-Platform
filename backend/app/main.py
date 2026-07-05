56a48a9be551b2aac92556c6cce2f7d2724df63c
tajirdev <mgenialfred671@gmail.com>
2026-07-05 12:43:39 +0300

    init database con

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
+from app.schemas import test
+from sqlalchemy.orm import Session
+from app.core.database import get_db,Base,engine
+
+from app.models import modeltest 
+Base.metadata.create_all(engine)
+
 
 app = FastAPI()
 
 @app.get('/')
 def hello():
     return {"message":"hello from docker3"}
+
+@app.post('/test')
+def test_db(request:test.Test,db:Session=Depends(get_db)):
+    test_new = modeltest.Test(name=request.name)
+    db.add(test_new)
+    db.commit()
+    db.refresh(test_new)
+    return test_new
+
+@app.get('/test')
+def get_test(db:Session=Depends(get_db)):
+    test_return = db.query(modeltest.Test).all()
+
+    if not test_return:
+        return{"message":"no in db"}
+    
+    return test_return
---
+++ backend/app/models/modeltest.py
@@ -0,0 +1,12 @@
+from app.core.database import Base
+from sqlalchemy import Column,Integer,String
+
+
+class Test(Base):
+  
+    __tablename__ = "test"
+
+    id = Column(Integer,primary_key=True)
+    name = Column(String)
+
+    
