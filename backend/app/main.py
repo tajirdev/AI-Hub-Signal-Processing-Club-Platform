be1629e35dd2426181e7aab528159933ba101c4c
agostinosasi3-rgb <agostinosasi3@gmail.com>
2026-08-05 17:34:54 +0300

    project modules

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI,Depends
 from app.schemas import test
 from sqlalchemy.orm import Session
 from app.core.database import get_db,Base,engine
-from app.routes import RouterUsers,loginroute,SubGroupRoute,MemberRouter
+from app.routes import RouterUsers,loginroute,SubGroupRoute,MemberRouter, project
 from app.core import seed_role
 
 
@@ -18,4 +18,4 @@ app.include_router(loginroute.router)
 app.include_router(seed_role.router)
 app.include_router(SubGroupRoute.router)
 app.include_router(MemberRouter.router)
-
+app.include_router(project.router)
