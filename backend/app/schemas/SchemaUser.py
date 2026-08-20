856c0882e4967d4c22d0e67a174fdaa40d53b96c
DOTTO MTUNDU <dottomtundu9@gmail.com>
2026-08-20 10:02:08 +0300

    Change github_link type to HttpUrl in SchemaUser
    small changes

--- backend/app/schemas/SchemaUser.py
+++ backend/app/schemas/SchemaUser.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel,EmailStr,HttpUrl
 from datetime import datetime
 from typing import Optional
 
@@ -10,7 +10,7 @@ class Users(BaseModel):
     password_hash: str
     phone : str
     bio : str
-    github_link : str
+    github_link : HttpUrl
 
 class UserResponse(BaseModel):
     id: int
@@ -20,7 +20,7 @@ class UserResponse(BaseModel):
     email: EmailStr
     phone: Optional[str] = None
     bio: Optional[str] = None
-    github_link: Optional[str] = None
+    github_link: Optional[httpurl] = None
     is_active: bool
     created_at: datetime
 
