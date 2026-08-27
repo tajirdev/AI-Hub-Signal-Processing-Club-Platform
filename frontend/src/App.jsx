75cf68d37b545886c83963930851e683752edbea
tajirdev <mgenialfred671@gmail.com>
2026-08-27 21:55:14 +0300

    small fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { ProjectsPage } from './features/projects/ProjectsPage';
 import { ProjectDetailsPage } from './features/projects/ProjectDetailsPage';
 import { SubgroupsPage } from './features/subgroups/SubgroupsPage';
 import { SubgroupDetailsPage } from './features/subgroups/SubgroupDetailsPage';
-﻿import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { PublicLayout } from './layouts/PublicLayout';
 import { AuthLayout } from './layouts/AuthLayout';
